1598367b5de2823c0ba4e553a5381ca2a8547477
Liuchenling <wssgcg1213@qq.com>
2014-02-19 20:09:28 +0800

    change style

--- siteSettings.js
+++ siteSettings.js
@@ -5,12 +5,15 @@
 var site = {
 	author: "wssgcg1213",
 	title: "Zero's CorNer",
-	subtitle: "ZeroLing",
+	subtitle: "Think Different!",
 	indexEmotionAmount: 5,
 		//主页显示的Emotion数量,etc
 	indexPostAmount: 3,
 	indexGalleryAmount: 3,
 	indexLinkAmount: 21,
+    siteinfo: "Powered By Node.js / Express / Mongo",
+    authorinfo: "Design By WangDaoDao，Code By ZeroLing，Passing HTML 5.0 & CSS 3.0",
+    copyright: "© 2014 Zeroling.com 版权所有",
 
 };
 
